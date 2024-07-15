312c9ed6d7ad856e319a21cf21ee77d91112f6eb
geovanarochamp <geovana.rocha.martins@gmail.com>
2024-07-15 16:04:02 -0300

    feat: add course detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { courseLessonsResolver } from './course/course-lessosn.resolver';
+import { CourseComponent } from './course/course.component';
+import { courseResolver } from './course/course.resolver';
 import { isUserAuthenticated } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LessonsComponent } from './lessons/lessons.component';
@@ -9,6 +12,15 @@ export const routes: Routes = [
     path: '',
     component: HomeComponent,
     canActivate: [isUserAuthenticated],
+  },
+  {
+    path: 'courses/:courseId',
+    component: CourseComponent,
+    canActivate: [isUserAuthenticated],
+    resolve: {
+      course: courseResolver,
+      lessons: courseLessonsResolver,
+    },
   },
   {
     path: 'login',
